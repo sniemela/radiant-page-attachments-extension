19764b6467f95540fa0691fa74798dab1d75f1d6
Jim Gay <jim@saturnflyer.com>
2010-05-03 23:56:35 -0400

    fix bad merge

--- db/migrate/20100305024408_add_indices.rb
+++ db/migrate/20100305024408_add_indices.rb
@@ -1,11 +1,7 @@
 class AddIndices < ActiveRecord::Migration
   def self.up
     add_index :page_attachments, :parent_id
-<<<<<<< HEAD
     add_index :page_attachments, [:page_id, :position], :unique => true
-=======
-    add_index :page_attachments, [:page_id, :position]
->>>>>>> b125303cc6f6370bfeef77eae67c3152ecadf907
   end
 
   def self.down
