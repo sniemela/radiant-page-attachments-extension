b93b6f7d72d10217e2024b35245adb6f0141a762
Jim Gay <jim@saturnflyer.com>
2010-05-04 00:57:55 -0400

    use resource controller

--- app/controllers/admin/page_attachments_controller.rb
+++ app/controllers/admin/page_attachments_controller.rb
@@ -1,25 +1,20 @@
-class Admin::PageAttachmentsController < ApplicationController
+class Admin::PageAttachmentsController < Admin::ResourceController
   
   def index
     @attachments = PageAttachment.all(:conditions => {:parent_id => nil}, :include => [:page], :order => 'title, filename').paginate(@pagination_parameters)
   end
+  
   def grid
     @attachments = PageAttachment.all(:conditions => {:parent_id => nil}, :include => [:page], :order => 'title, filename').paginate(@pagination_parameters)
   end
   
-  def edit
-    @page_attachment = PageAttachment.find(params[:id])
-  end
   def update
     @page_attachment = PageAttachment.find(params[:id])
     params[:page_attachment].each do |k,v|
       @page_attachment[k] = v
     end
-    if @page_attachment.save!
-      redirect_to admin_page_attachments_url
-    else
-      render :edit
-    end
+    @page_attachment.save!
+    response_for :update
   end
   
 end
