b2271b0e424bf1c3585f63f9d6e6fd30ba5d3ef9
Jim Gay <jim@saturnflyer.com>
2010-05-03 23:48:13 -0400

    alt and title attributes for sample code

--- app/helpers/admin/page_attachments_helper.rb
+++ app/helpers/admin/page_attachments_helper.rb
@@ -14,11 +14,15 @@ module Admin::PageAttachmentsHelper
     unless @page
       url_scope = " url='#{attachment.page.url}'"
     end
+    sample_start = h(%{<r:attachment name='#{attachment.filename}'#{url_scope}>})
+    sample_middle = h(%{<r:link title='#{escape_javascript(attachment.title)}' />})
+    sample_end = h(%{</r:attachment>})
+    
     if attachment.filename.match(/\.(jpg|gif|png|jpeg|tiff?)$/)
-      code = h(%{<r:attachment name='#{attachment.filename}'#{url_scope}><r:image/></r:attachment>})
-    else
-      code = h(%{<r:attachment name='#{attachment.filename}'#{url_scope}><r:link/></r:attachment>})
-    end        
-    tag("input", {:value => "#{code}", :type => 'text', :value => code, :size => 60})
+      sample_middle = h(%{<r:image alt='#{escape_javascript(attachment.title)}'/>})
+    end
+    
+    code = sample_start + sample_middle + sample_end
+    tag("input", {:type => 'text', :value => code, :size => 60})
   end
 end
