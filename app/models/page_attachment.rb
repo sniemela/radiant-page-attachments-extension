2e8bc3f772d91c7eb919709f042da95376b5480f
Simo Niemelä <simo.niemela@gmail.com>
2010-05-17 11:43:37 +0300

    Add position as attr_accessible

--- app/models/page_attachment.rb
+++ app/models/page_attachment.rb
@@ -13,7 +13,7 @@ class PageAttachment < ActiveRecord::Base
              :foreign_key => 'updated_by'
   belongs_to :page
 
-  attr_accessible :title, :description
+  attr_accessible :title, :description, :position
 
   def short_filename(wanted_length = 15, suffix = ' ...')
           (self.filename.length > wanted_length) ? (self.filename[0,(wanted_length - suffix.length)] + suffix) : self.filename
