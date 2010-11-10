fd5f6cd8d576a8b85a54bc4cfc5e6034c2fd9ba0
Simo Niemelä <simo.niemela@gmail.com>
2010-11-10 23:08:16 +0200

    @pagination_parameters is a method

--- app/controllers/admin/page_attachments_controller.rb
+++ app/controllers/admin/page_attachments_controller.rb
@@ -1,11 +1,11 @@
 class Admin::PageAttachmentsController < Admin::ResourceController
   
   def index
-    @attachments = PageAttachment.all(:conditions => {:parent_id => nil}, :include => [:page], :order => 'title, filename').paginate(@pagination_parameters)
+    @attachments = PageAttachment.all(:conditions => {:parent_id => nil}, :include => [:page], :order => 'title, filename').paginate(pagination_parameters)
   end
   
   def grid
-    @attachments = PageAttachment.all(:conditions => {:parent_id => nil}, :include => [:page], :order => 'title, filename').paginate(@pagination_parameters)
+    @attachments = PageAttachment.all(:conditions => {:parent_id => nil}, :include => [:page], :order => 'title, filename').paginate(pagination_parameters)
   end
   
   def update
